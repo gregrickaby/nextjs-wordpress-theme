4c7cc3d8b3ee013d3729384dc74c4b2ad10b6918
Greg Rickaby <gregrickaby@gmail.com>
2023-10-21 11:02:49 -0500

    check for const. if none, set one.

--- functions.php
+++ functions.php
@@ -6,3 +6,10 @@
  * @package nextjs-wordpress-theme
  * @since 1.0.0
  */
+
+/**
+ * Check for the HEADLESS_FRONTEND_URL constant.
+ */
+if ( ! defined( 'HEADLESS_FRONTEND_URL' ) ) {
+	define( 'HEADLESS_FRONTEND_URL', 'https://nextjswp.com' );
+}
